e1a5dd35e598db9379826daa32c772e5b4e02f8b
ouali99 <oualiob2@gmail.com>
2024-03-28 22:56:11 -0400

    ajout des images pour la view panier/checkout

--- views/paniers/checkout.php
+++ views/paniers/checkout.php
@@ -158,7 +158,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/instagram/insta-1.jpg">
+                        <div class="instagram__item set-bg" data-setbg="<?= URI."assets/front/img/instagram/insta-1.jpg"?>">
                             <div class="instagram__text">
                                 <i class="fa fa-instagram"></i>
                                 <a href="#">@ ashion_shop</a>
@@ -166,7 +166,7 @@
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/instagram/insta-2.jpg">
+                        <div class="instagram__item set-bg" data-setbg="<?= URI."assets/front/img/instagram/insta-2.jpg"?>">
                             <div class="instagram__text">
                                 <i class="fa fa-instagram"></i>
                                 <a href="#">@ ashion_shop</a>
@@ -174,7 +174,7 @@
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/instagram/insta-3.jpg">
+                        <div class="instagram__item set-bg" data-setbg="<?= URI."assets/front/img/instagram/insta-3.jpg"?>">
                             <div class="instagram__text">
                                 <i class="fa fa-instagram"></i>
                                 <a href="#">@ ashion_shop</a>
@@ -182,7 +182,7 @@
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/instagram/insta-4.jpg">
+                        <div class="instagram__item set-bg" data-setbg="<?= URI."assets/front/img/instagram/insta-4.jpg"?>">
                             <div class="instagram__text">
                                 <i class="fa fa-instagram"></i>
                                 <a href="#">@ ashion_shop</a>
@@ -190,7 +190,7 @@
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/instagram/insta-5.jpg">
+                        <div class="instagram__item set-bg" data-setbg="<?= URI."assets/front/img/instagram/insta-5.jpg"?>">
                             <div class="instagram__text">
                                 <i class="fa fa-instagram"></i>
                                 <a href="#">@ ashion_shop</a>
@@ -198,7 +198,7 @@
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                        <div class="instagram__item set-bg" data-setbg="img/instagram/insta-6.jpg">
+                        <div class="instagram__item set-bg" data-setbg="<?= URI."assets/front/img/instagram/insta-6.jpg"?>">
                             <div class="instagram__text">
                                 <i class="fa fa-instagram"></i>
                                 <a href="#">@ ashion_shop</a>
