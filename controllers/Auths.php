6825b7163360556a51a027f530af968e770606d0
ouali99 <oualiob2@gmail.com>
2024-04-08 23:06:41 -0400

    ajout de auth controllers et models

--- controllers/Auths.php
+++ controllers/Auths.php
@@ -8,10 +8,71 @@ class Auths extends Controllers{
 
     public function login(){
 
+        if(isset($_SESSION["utilisateur"])){
+            header("Location: ".URI."Koli-project/index");
+        }
+
+        if(isset($_POST["submit"])){
+            if(!$this->estVide($_POST)){
+                $mdp = $_POST["mot_de_passe"];
+                $auth = new Auth();
+                $auth->email = $_POST["email"];
+                $user = $auth->findByEmail();
+                if($user){
+                    if(password_verify($mdp, $user->mot_de_passe)){
+                        unset($user->mot_de_passe);
+                        $_SESSION["utilisateur"] = $user;
+                        header("Location: ".URI."Koli-project/index");
+                    }else{
+                        $erreurs["message"] = "Mot de passe invalid";
+                        $this->render("login", $erreurs);
+                        return;
+                    }
+                }else{
+                    $erreurs["message"] = "User don't exist";
+                    $this->render("login", $erreurs);
+                    return;
+                }
+            }else{
+                $erreurs["message"] = "Remplir tous les champs svp";
+                $this->render("login", $erreurs);
+                return;
+            }
+        }
         $this->render("login");
     }
 
     public function register(){
+        if(isset($_SESSION["utilisateur"])){
+            $this->render("index", [], "Koli-project");
+        }
+        if(isset($_POST['enregistre'])){
+            if(!$this->estVide($_POST)){
+                $mdp = $_POST['mot_de_passe'];
+                $cmdp = $_POST['c_mot_de_passe'];
+                if($mdp === $cmdp){
+                    unset($_POST['enregistre']);
+                    unset($_POST['c_mot_de_passe']);
+                    $_POST['mot_de_passe'] = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
+                    $auth = new Auth();
+                    $auth->saveUser($_POST);
+                }
+                var_dump($_SESSION["utilisateur"]);
+            }
+        }
         $this->render("register");
+    }
+
+    public function deconnexion(){
+        unset($_SESSION["utilisateur"]);
+        header("Location: ".URI."Koli-project/index");
+    }
+
+    public function index(){
+        var_dump($_SESSION["utilisateur"]);
+        if(!isset($_COOKIE["utilisateur"])){
+            setcookie("utilisateur","utilisateur",time()+(60 * 60 * 24 * 2), "/");
+    }
+    echo"je suis index";
     }
 }
